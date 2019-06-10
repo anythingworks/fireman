c0d28a1b62ab6b29bc9584661e910a2bd2662ab1
flynn <flynnmeyer1@gmail.com>
2019-06-09 21:45:45 -0700

    settings button

--- scenes/Astroid.gd
+++
@@ -1,6 +0,0 @@
-extends KinematicBody2D
-
-var velocity = Vector2()
-
-func _physics_process(delta):
-	velocity.x = -10
--- scenes/distance till moon.gd
+++
@@ -1,8 +0,0 @@
-extends RichTextLabel
-var distance = 1500
-
-func _process(delta):
-	set_text(str(distance) + 'KM')
-	
-func _on_Timer4distancce_timeout():
-	distance += -1
